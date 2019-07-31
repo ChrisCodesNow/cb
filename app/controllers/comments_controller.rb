65a1d3ae647eb8734ffb1813c7c78af11b518649
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-07-30 21:03:38 -0500

    Complete showing articles

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,23 @@
 class CommentsController < ApplicationController
+    def show
+        @comment = Comment.find(params[:id])
+    end
+    
+    
     def new
         
     end
 
     
     def create
-        render plain: params[:comment].inspect
+        @comment = Comment.new(comment_params)    
+
+        @comment.save
+        redirect_to @comment
     end
+
+    private
+        def comment_params
+            params.require(:comment).permit(:name, :comment)
+        end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_014057) do
+ActiveRecord::Schema.define(version: 2019_07_31_015129) do
 
   create_table "articles", force: :cascade do |t|
+    t.string "name"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
     t.string "name"
     t.string "comment"
     t.datetime "created_at", null: false
